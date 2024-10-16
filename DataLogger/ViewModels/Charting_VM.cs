5d689bd491025b986a8ca0c03c8649a7d9498b63
NisbetLuke <luke.nisbet@bobst.com>
2024-10-16 16:29:53 +0100

    Charts working but need styling

--- DataLogger/ViewModels/Charting_VM.cs
+++ DataLogger/ViewModels/Charting_VM.cs
@@ -7,27 +7,67 @@ using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
 using System.Collections.ObjectModel;
 using SQLight_Database;
+using LiveChartsCore.Defaults;
 
 namespace DataLogger.ViewModels
 {
     internal class Charting_VM
     {
         public ObservableCollection<ISeries> Series { get; set; }
+        public ObservableCollection<Axis> XAxes { get; set; }
+        public ObservableCollection<Axis> YAxes { get; set; }
         public Charting_VM()
         {
-            // Map data to series
-            var chartPoints = LogsTable.Logs
-                .Select(log => new { X = log.Date.ToDateTime(TimeOnly.MinValue).ToOADate(), Y = (double)log.Value1 })
-                .ToList();
-
-            Series = new ObservableCollection<ISeries>
+            
+            XAxes = new ObservableCollection<Axis>
             {
-                new LineSeries<double>
+                new Axis
                 {
-                    Values = chartPoints.Select(p => p.Y).ToList(), // Y-values (Value1)
-                    // Mapping is not necessary since we are using basic data types
+                    Name = "Date",
+                    Labeler = (value) => value.ToString(),
+                    LabelsRotation = 15,
                 }
             };
+            YAxes = new ObservableCollection<Axis>
+            {
+                new Axis
+                {
+                    Name = "Value1",
+                    // Now the Y axis we will display labels as currency
+                    // LiveCharts provides some common formatters
+                    // in this case we are using the currency formatter.
+                    //Labeler = Labelers.Currency 
+
+                    // you could also build your own currency formatter
+                    // for example:
+                    // Labeler = (value) => value.ToString("C")
+
+                    // But the one that LiveCharts provides creates shorter labels when
+                    // the amount is in millions or trillions
+                }
+            };
+
+            Series = [];
+            foreach (var exercise in ExerciseTable.Exercises)
+            {
+                // Map the logs into X (Date) and Y (Value1)
+                var chartPoints = LogsTable.Logs
+                    .Where(log => log.Exercise == exercise)  // Filter by exercise
+                    .Select(log => new DateTimePoint
+                    {
+                        DateTime = log.Date.ToDateTime(TimeOnly.MinValue), // X-axis (date)
+                        Value = (double)log.Value1 // Y-axis (value1)
+                    })
+                    .ToList();
+
+                // Create a new line series for the exercise
+                Series.Add(new LineSeries<DateTimePoint>
+                {
+                    Values = chartPoints,  // Bind the chart points to the series
+                    Name = exercise.Name,   // Label the series by exercise name
+                    XToolTipLabelFormatter = point => $"{exercise.Name}: {point.PrimaryValue:N2} on {point.SecondaryValue:MM/dd/yyyy}"
+                });
+            }
         }
     }
 }
